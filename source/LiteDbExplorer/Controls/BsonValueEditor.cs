d4b8faf22dcb2aa03700338141cc95809a21d52e
SunTai（三台） <a44281071@sina.com>
2018-07-16 14:33:14 +0800

    can edit guid field.

--- source/LiteDbExplorer/Controls/BsonValueEditor.cs
+++ source/LiteDbExplorer/Controls/BsonValueEditor.cs
@@ -1,165 +1,172 @@
 ﻿using LiteDB;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using LiteDbExplorer.Converters;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using LiteDbExplorer.Converters;
-using Xceed.Wpf.Toolkit;
 using System.Windows.Controls.Primitives;
-using System.ComponentModel;
+using System.Windows.Data;
+using Xceed.Wpf.Toolkit;
 
 namespace LiteDbExplorer.Controls
 {
-    public class BsonValueEditor
+  public class BsonValueEditor
+  {
+    public static FrameworkElement GetBsonValueEditor(string bindingPath, BsonValue bindingValue, object bindingSource, bool readOnly)
     {
-        public static FrameworkElement GetBsonValueEditor(string bindingPath, BsonValue bindingValue, object bindingSource, bool readOnly)
+      var binding = new Binding()
+      {
+        Path = new PropertyPath(bindingPath),
+        Source = bindingSource,
+        Mode = BindingMode.TwoWay,
+        Converter = new BsonValueToNetValueConverter(),
+        UpdateSourceTrigger = UpdateSourceTrigger.Explicit
+      };
+      var guidBinding = new Binding()
+      {
+        Path = new PropertyPath(bindingPath),
+        Source = bindingSource,
+        Mode = BindingMode.TwoWay,
+        Converter = new BsonValueToGuidConverter(),
+        UpdateSourceTrigger = UpdateSourceTrigger.Default
+      };
+      if (bindingValue.IsArray)
+      {
+        var button = new Button()
         {
-            var binding = new Binding()
-            {
-                Path = new PropertyPath(bindingPath),
-                Source = bindingSource,
-                Mode = BindingMode.TwoWay,
-                Converter = new BsonValueToNetValueConverter(),
-                UpdateSourceTrigger = UpdateSourceTrigger.Explicit
-            };
+          Content = "Array"
+        };
 
-            if (bindingValue.IsArray)
-            {
-                var button = new Button()
-                {
-                    Content = "Array"
-                };
+        button.Click += (s, a) =>
+        {
+          var arrayValue = bindingValue as BsonArray;
+          var window = new Windows.ArrayViewer(arrayValue, readOnly)
+          {
+            Owner = Application.Current.MainWindow
+          };
 
-                button.Click += (s, a) =>
-                {
-                    var arrayValue = bindingValue as BsonArray;
-                    var window = new Windows.ArrayViewer(arrayValue, readOnly)
-                    {
-                        Owner = Application.Current.MainWindow
-                    };
+          if (window.ShowDialog() == true)
+          {
+            arrayValue.Clear();
+            arrayValue.AddRange(window.EditedItems);
+          }
+        };
 
-                    if (window.ShowDialog() == true)
-                    {
-                        arrayValue.Clear();
-                        arrayValue.AddRange(window.EditedItems);
-                    }
-                };
+        return button;
+      }
+      else if (bindingValue.IsBoolean)
+      {
+        var check = new CheckBox()
+        {
+          IsEnabled = !readOnly
+        };
 
-                return button;
-            }
-            else if (bindingValue.IsBoolean)
-            {
-                var check = new CheckBox()
-                {
-                    IsEnabled = !readOnly
-                };
+        check.SetBinding(ToggleButton.IsCheckedProperty, binding);
+        return check;
+      }
+      else if (bindingValue.IsDateTime)
+      {
+        var datePicker = new DateTimePicker()
+        {
+          TextAlignment = TextAlignment.Left,
+          IsReadOnly = readOnly
+        };
 
-                check.SetBinding(ToggleButton.IsCheckedProperty, binding);
-                return check;
-            }
-            else if (bindingValue.IsDateTime)
-            {
-                var datePicker = new DateTimePicker()
-                {
-                    TextAlignment = TextAlignment.Left,
-                    IsReadOnly = readOnly
-                };
+        datePicker.SetBinding(DateTimePicker.ValueProperty, binding);
+        return datePicker;
+      }
+      else if (bindingValue.IsDocument)
+      {
+        var button = new Button()
+        {
+          Content = "Document"
+        };
 
-                datePicker.SetBinding(DateTimePicker.ValueProperty, binding);
-                return datePicker;
-            }
-            else if (bindingValue.IsDocument)
-            {
-                var button = new Button()
-                {
-                    Content = "Document"
-                };
+        button.Click += (s, a) =>
+        {
+          var window = new Windows.DocumentViewer(bindingValue as BsonDocument, readOnly)
+          {
+            Owner = Application.Current.MainWindow
+          };
 
-                button.Click += (s, a) =>
-                {
-                    var window = new Windows.DocumentViewer(bindingValue as BsonDocument, readOnly)
-                    {
-                        Owner = Application.Current.MainWindow
-                    };
+          window.ShowDialog();
+        };
 
-                    window.ShowDialog();
-                };
+        return button;
+      }
+      else if (bindingValue.IsDouble)
+      {
+        var numberEditor = new DoubleUpDown()
+        {
+          TextAlignment = TextAlignment.Left,
+          IsReadOnly = readOnly
+        };
 
-                return button;
-            }
-            else if (bindingValue.IsDouble)
-            {
-                var numberEditor = new DoubleUpDown()
-                {
-                    TextAlignment = TextAlignment.Left,
-                    IsReadOnly = readOnly
-                };
+        numberEditor.SetBinding(DoubleUpDown.ValueProperty, binding);
+        return numberEditor;
+      }
+      else if (bindingValue.IsInt32)
+      {
+        var numberEditor = new IntegerUpDown()
+        {
+          TextAlignment = TextAlignment.Left,
+          IsReadOnly = readOnly
+        };
 
-                numberEditor.SetBinding(DoubleUpDown.ValueProperty, binding);
-                return numberEditor;
-            }
-            else if (bindingValue.IsInt32)
-            {
-                var numberEditor = new IntegerUpDown()
-                {
-                    TextAlignment = TextAlignment.Left,
-                    IsReadOnly = readOnly
-                };
+        numberEditor.SetBinding(IntegerUpDown.ValueProperty, binding);
+        return numberEditor;
+      }
+      else if (bindingValue.IsInt64)
+      {
+        var numberEditor = new LongUpDown()
+        {
+          TextAlignment = TextAlignment.Left,
+          IsReadOnly = readOnly
+        };
 
-                numberEditor.SetBinding(IntegerUpDown.ValueProperty, binding);
-                return numberEditor;
-            }
-            else if (bindingValue.IsInt64)
-            {
-                var numberEditor = new LongUpDown()
-                {
-                    TextAlignment = TextAlignment.Left,
-                    IsReadOnly = readOnly
-                };
+        numberEditor.SetBinding(LongUpDown.ValueProperty, binding);
+        return numberEditor;
+      }
+      else if (bindingValue.IsString)
+      {
+        var stringEditor = new TextBox()
+        {
+          IsReadOnly = readOnly,
+          AcceptsReturn = true
+        };
 
-                numberEditor.SetBinding(LongUpDown.ValueProperty, binding);
-                return numberEditor;
-            }
-            else if (bindingValue.IsString)
-            {
-                var stringEditor = new TextBox()
-                {
-                    IsReadOnly = readOnly,
-                    AcceptsReturn = true
-                };
+        stringEditor.SetBinding(TextBox.TextProperty, binding);
+        return stringEditor;
+      }
+      else if (bindingValue.IsBinary)
+      {
+        var text = new TextBlock()
+        {
+          Text = "[Binary Data]"
+        };
 
-                stringEditor.SetBinding(TextBox.TextProperty, binding);
-                return stringEditor;
-            }
-            else if (bindingValue.IsBinary)
-            {
-                var text = new TextBlock()
-                {
-                    Text = "[Binary Data]"
-                };
+        return text;
+      }
+      else if (bindingValue.IsObjectId)
+      {
+        var text = new TextBlock()
+        {
+          Text = bindingValue.AsString,
+          IsEnabled = false
+        };
 
-                return text;
-            }
-            else if (bindingValue.IsObjectId)
-            {
-                var text = new TextBlock()
-                {
-                    Text = bindingValue.AsString,
-                    IsEnabled = false
-                };
-
-                return text;
-            }
-            else
-            {
-                var stringEditor = new TextBox();
-                stringEditor.SetBinding(TextBox.TextProperty, binding);
-                return stringEditor;
-            }
-        }
+        return text;
+      }
+      else if (bindingValue.IsGuid)
+      {
+        var stringEditor = new TextBox();
+        stringEditor.SetBinding(TextBox.TextProperty, guidBinding);
+        return stringEditor;
+      }
+      else
+      {
+        var stringEditor = new TextBox();
+        stringEditor.SetBinding(TextBox.TextProperty, binding);
+        return stringEditor;
+      }
     }
+  }
 }
---
+++ source/LiteDbExplorer/Converters/BsonValueToGuidConverter.cs
@@ -0,0 +1,35 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace LiteDbExplorer.Converters
+{
+  [ValueConversion(typeof(Guid?), typeof(string))]
+  public class BsonValueToGuidConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      return value == null
+        ? null
+        : ((BsonValue)value).AsGuid.ToString();
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      string vvv = value?.ToString();
+      if (!String.IsNullOrWhiteSpace(vvv))
+      {
+        if (Guid.TryParse(vvv, out Guid g))
+        {
+          return new BsonValue(g);
+        }
+      }
+      return null;
+    }
+  }
+}
