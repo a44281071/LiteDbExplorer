bbb468b66dbfb570fdf43a1405d54cf04cbc499e
SunTai（三台） <a44281071@sina.com>
2018-11-22 14:34:01 +0800

    move writeable work file to application data folder.

--- source/LiteDbExplorer/Config.cs
+++ source/LiteDbExplorer/Config.cs
@@ -11,79 +11,45 @@ using System.Threading.Tasks;
 
 namespace LiteDbExplorer
 {
-    public class Config
+  public class Config
+  {
+    public static string UpdateDataUrl { get { return ConfigurationManager.AppSettings["UpdateUrl"]; } }
+    public static string IssuesUrl { get { return ConfigurationManager.AppSettings["IssuesUrl"]; } }
+    public static string HomepageUrl { get { return ConfigurationManager.AppSettings["HomepageUrl"]; } }
+    public static string ReleasesUrl { get { return ConfigurationManager.AppSettings["ReleasesUrl"]; } }
+    public static bool IsPortable { get { return !File.Exists(Paths.UninstallerPath); } }
+
+    public static void ConfigureLogger()
     {
-        public static string UpdateDataUrl
-        {
-            get
-            {
-                return ConfigurationManager.AppSettings["UpdateUrl"];
-            }
-        }
-
-        public static string IssuesUrl
-        {
-            get
-            {
-                return ConfigurationManager.AppSettings["IssuesUrl"];
-            }
-        }
-
-        public static string HomepageUrl
-        {
-            get
-            {
-                return ConfigurationManager.AppSettings["HomepageUrl"];
-            }
-        }
-
-        public static string ReleasesUrl
-        {
-            get
-            {
-                return ConfigurationManager.AppSettings["ReleasesUrl"];
-            }
-        }
-        
-        public static bool IsPortable
-        {
-            get
-            {
-                return !File.Exists(Paths.UninstallerPath);
-            }
-        }
-
-        public static void ConfigureLogger()
-        {
-            var config = new LoggingConfiguration();
+      var config = new LoggingConfiguration();
 #if DEBUG
-            var consoleTarget = new ColoredConsoleTarget()
-            {
-                Layout = @"${logger}:${message}${exception}"
-            };
+      var consoleTarget = new ColoredConsoleTarget()
+      {
+        Layout = @"${logger}:${message}${exception}"
+      };
 
-            config.AddTarget("console", consoleTarget);
+      config.AddTarget("console", consoleTarget);
 
-            var rule1 = new LoggingRule("*", LogLevel.Debug, consoleTarget);
-            config.LoggingRules.Add(rule1);
+      var rule1 = new LoggingRule("*", LogLevel.Debug, consoleTarget);
+      config.LoggingRules.Add(rule1);
 #endif
-            var fileTarget = new FileTarget()
-            {
-                FileName = Path.Combine(Paths.ProgramFolder, "explorer.log"),
-                Layout = "${longdate}|${level:uppercase=true}:${message}${exception:format=toString}",
-                KeepFileOpen = false,
-                ArchiveFileName = Path.Combine(Paths.ProgramFolder, "explorer.{#####}.log"),
-                ArchiveAboveSize = 4096000,
-                ArchiveNumbering = ArchiveNumberingMode.Sequence,
-                MaxArchiveFiles = 2
-            };
+      var fileTarget = new FileTarget()
+      {
+        FileName = Path.Combine(Paths.ApplicationDataFolder, "explorer.log"),
+        Layout = "${longdate}|${level:uppercase=true}:${message}${exception:format=toString}",
+        KeepFileOpen = false,
+        ArchiveFileName = Path.Combine(Paths.ApplicationDataFolder, "explorer.{#####}.log"),
+        ArchiveAboveSize = 4096000,
+        ArchiveNumbering = ArchiveNumberingMode.Sequence,
+        MaxArchiveFiles = 2
+      };
 
-            config.AddTarget("file", fileTarget);
+      config.AddTarget("file", fileTarget);
 
-            var rule2 = new LoggingRule("*", LogLevel.Debug, fileTarget);
-            config.LoggingRules.Add(rule2);
+      var rule2 = new LoggingRule("*", LogLevel.Debug, fileTarget);
+      config.LoggingRules.Add(rule2);
 
-            LogManager.Configuration = config;
-        }
+      LogManager.Configuration = config;
     }
+  }
 }
